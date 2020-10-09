cec566eb57d7c03b3c63f942d4a051986c97d1fc
GreyStinger <greystinger@gmail.com>
2020-10-09 10:12:41 +0200

    First Fully Funtional Test

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: 'post-list.component.html',
+  styleUrls: ['post-list.component.css'],
+})
+
+export class PostListComponent implements OnInit {
+
+  @Input() posts: Post[] = []
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+  }
+
+}
