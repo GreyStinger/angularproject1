fd583830dccbad3069af89ac4bda189dad298235
JaydenAndrews <jaydenandrews010@gmail.com>
2020-10-10 13:40:11 +0200

    Reset Form Save

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -20,7 +20,7 @@ export class PostCreateComponent {
       return
     }
      this.postsService.addPost(form.value.title, form.value.content);
-     form.resetForm;
+     form.resetForm();
   }
 
 }
