d2134af436fbb97941749cda02e4b724371034e7
GreyStinger <jaydenandrews010@gmail.com>
2020-10-28 01:16:39 +0200

    Directory Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostCreateComponent } from './components/posts/post-create/post-create.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostListComponent } from './components/posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
